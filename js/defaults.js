667c2ad44836dd41cf194c02771b7ecc31532847
obozdag <obozdag@gmail.com>
2025-11-16 08:12:48 +0100

    settings.js file renamed to defaults.js

---
+++ js/defaults.js
@@ -0,0 +1,81 @@
+defaultBgColor    = 'cornsilk';
+defaultColor      = 'brown';
+defaultFontFamily = 'EasyArabic';
+defaultFontSize   = '36px';
+bookmarksLength   = 5;
+
+languages = {
+	'tr' : 'Türkçe',
+	'en' : 'English',
+};
+
+ajza = {}; for(i=1; i<=30;i++){ajza['j'+i] = i;};
+
+fontSizes = {}; for(i=16; i<=64;i+=2){fontSizes[i+'px'] = i;};
+
+fontFamilies  = {
+	'EasyArabic' : 'EasyArabic',
+	'Lateef' : 'Lateef',
+};
+
+colors = {
+	'brown'          : 'Brown',
+	'cadetblue'      : 'Cadetblue',
+	'chocolate'      : 'Chocolate',
+	'coral'          : 'Coral',
+	'cornflowerblue' : 'Cornflowerblue',
+	'crimson'        : 'Crimson',
+	'darkblue'       : 'Darkblue',
+	'darkcyan'       : 'Darkcyan',
+	'darkgoldenrod'  : 'Darkgoldenrod',
+	'darkmagenta'    : 'Darkmagenta',
+	'darkolivegreen' : 'Darkolivegreen',
+	'darkorange'     : 'Darkorange',
+	'darkred'        : 'Darkred',
+	'darkslategray'  : 'Darkslategray',
+	'deeppink'       : 'Deeppink',
+	'midnightblue'   : 'Midnightblue',
+	'orangered'      : 'Orangered',
+	'red'            : 'Red',
+	'royalblue'      : 'Royalblue',
+	'seagreen'       : 'Seagreen',
+	'steelblue'      : 'Steelblue',
+	'teal'           : 'Teal',
+	'#3a86ff'        : 'Nylonblue',
+	'#863dec'        : 'Overreact',
+	'#ff006e'        : 'Respark',
+	'#00b4be'        : 'Babeblue',
+	'#c77dff'        : 'Venomoth',
+	'#f160b7'        : 'Heavenlylove',
+	'#00b7f4'        : 'Summervacation',
+	'#00f5d4'        : 'Sunshinyblue',
+};
+
+bgColors = {
+	'cornsilk'       : 'Cornsilk',
+	'floralwhite'    : 'Floralwhite',
+	'ghostwhite'     : 'Ghostwhite',
+	'honeydew'       : 'Honeydew',
+	'ivory'          : 'Ivory',
+	'khaki'          : 'Khaki',
+	'lavender'       : 'Lavender',
+	'lavenderblush'  : 'Lavenderblush',
+	'lemonchiffon'   : 'Lemonchiffon',
+	'lightblue'      : 'Lightblue',
+	'lightcyan'      : 'Lightcyan',
+	'lightpink'      : 'Lightpink',
+	'lightskyblue'   : 'Lightskyblue',
+	'lightyellow'    : 'Lightyellow',
+	'linen'          : 'Linen',
+	'mintcream'      : 'Mintcream',
+	'seashell'       : 'Seashell',
+	'white'          : 'White',
+	'#3a86ff'        : 'Nylonblue',
+	'#863dec'        : 'Overreact',
+	'#ff006e'        : 'Respark',
+	'#00b4be'        : 'Babeblue',
+	'#c77dff'        : 'Venomoth',
+	'#f160b7'        : 'Heavenlylove',
+	'#00b7f4'        : 'Summervacation',
+	'#00f5d4'        : 'Sunshinyblue',
+};
